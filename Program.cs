4512c886142561717a0235463338cc759bd1ee3d
zelinskyiak24-maker <zelinskyi_ak24@nuwm.edu.ua>
2026-01-15 21:18:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,54 +1,37 @@
 using System;
 
-class CubicPolynomial
-{
-    private double a3, a2, a1, a0;
-
-    public CubicPolynomial(double a3, double a2, double a1, double a0)
+    class Program
     {
-        this.a3 = a3;
-        this.a2 = a2;
-        this.a1 = a1;
-        this.a0 = a0;
-    }
-
-    public double Value(double x)
-    {
-        return a3 * x * x * x + a2 * x * x + a1 * x + a0;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        CubicPolynomial[] polynomials =
+        static void Main()
         {
-            new CubicPolynomial(1, -2, 3, -1),
-            new CubicPolynomial(-1, 0, 2, 4)
-        };
+            Console.Write("Введіть сторону a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Введіть a: ");
-        double a = double.Parse(Console.ReadLine());
+            Console.Write("Введіть сторону b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Введіть b: ");
-        double b = double.Parse(Console.ReadLine());
+            Console.Write("Введіть сторону c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Введіть ε: ");
-        double eps = double.Parse(Console.ReadLine());
-
-        for (int i = 0; i < polynomials.Length; i++)
-        {
-            double min = polynomials[i].Value(a);
-
-            for (double x = a; x <= b; x += eps)
+            if (a <= 0 || b <= 0 || c <= 0)
             {
-                double value = polynomials[i].Value(x);
-                if (value < min)
-                    min = value;
+                Console.WriteLine("Усі сторони мають бути більші за 0!");
             }
+            else if (!(a + b > c && a + c > b && b + c > a))
+            {
+                Console.WriteLine("Трикутник з такими сторонами не існує!");
+            }
+            else if (!(a == b || a == c || b == c))
+            {
+                Console.WriteLine("Трикутник не є рівнобедреним!");
+            }
+            else
+            {
+                double p = (a + b + c) / 2;
+                double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
 
-            Console.WriteLine($"Мінімальне значення {i + 1}-го многочлена: {min}");
+                Console.WriteLine($"Площа трикутника: {s}");
+            }
         }
     }
-}
+
